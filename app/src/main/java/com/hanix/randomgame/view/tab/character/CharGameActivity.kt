6dd3329cadf3ab8ca2ffddf4abce570a608f84fb
johyewon <ixxwxfjlwf@gmail.com>
2021-11-25 13:58:02 +0900

    activity export false

--- app/src/main/java/com/hanix/randomgame/view/tab/character/CharGameActivity.kt
+++ app/src/main/java/com/hanix/randomgame/view/tab/character/CharGameActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hanix.randomgame.R
 import com.hanix.randomgame.common.app.GLog
+import java.util.*
 
 class CharGameActivity : AppCompatActivity() {
 
     private var headCount = 0
     private var turnCount = 0
+
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +20,28 @@ class CharGameActivity : AppCompatActivity() {
         getCount()
     }
 
+    override fun onStop() {
+        super.onStop()
+
+        stopTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        stopTimer()
+    }
+
     private fun getCount() {
         headCount = intent.getIntExtra("headCount", 0)
         turnCount = intent.getIntExtra("turnCount", 0)
 
         GLog.d("headCount : $headCount , turnCount : $turnCount")
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+        // set text ="중지"
     }
 
 }
